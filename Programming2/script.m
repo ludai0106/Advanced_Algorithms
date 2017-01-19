68068ecf636ff853af3bac28e017040f3596199a
YuxuanKang <YuxuanKang1221@gmail.com>
2017-01-19 17:17:53 +0100

    Add rouding algorithm, return value: upper=upperbound, lower=lowerbound, n= number of nodes, delta_t=runtime of SDP, delta_ta= runtime of rounding algorithm, accuracy= accuracy of SDP

--- Programming2/script.m
+++ Programming2/script.m
@@ -1,15 +1,29 @@
-Directory='F:\TUDelft\IN4301 Advanced Algorithms\Programming2\Instances0.5\';
-ts=[];
+Directory='F:\TUDelft\IN4301 Advanced Algorithms\Programming2\Programming2\Instances0.5\';
+uppers=[];
+lowers=[];
+ns=[];
 Xs=[];
-Times=[];
+Delta_ts=[];
+Delta_tas=[];
 As=[];
 FileNames=dir(Directory);
 for i=3:size(FileNames,1)
     path=strcat(Directory,FileNames(i,1).name)
-    [t,X,time,accuracy]=maxcut(path);
-    ts=[ts t];
-    Times=[Times time];
+    [upper,lower,n,X,Delta_t,Delta_ta,accuracy]=maxcut(path,100);
+    ns=[ns n]
+    uppers=[uppers upper];
+    lowers=[lowers,lower];
+    Delta_ts=[Delta_ts Delta_t];
+    Delta_tas=[Delta_tas Delta_ta];
     As=[As accuracy];
 end
-x=[5 10 20 30 40 50 60 70]
-plot(x,Times);
+[sorted, indices] = sort(ns)
+ns=ns(indices)
+uppers=uppers(indices)
+lowers=lowers(indices)
+Delta_ts=Delta_ts(indices);
+Delta_tas=Delta_tas(indices);
+figure(1)
+plot(ns,uppers,ns,lowers);
+figure(2)
+plot(ns,Delta_ts,ns,Delta_tas);
